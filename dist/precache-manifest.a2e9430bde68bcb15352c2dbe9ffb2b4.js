a34ce93eef4bd5583bdb1f472c01ae8423635ae4
Dimitry Likhnenko <shymaher.d@gmail.com>
2021-02-09 16:16:20 +0200

    Version 1.3

---
+++ dist/precache-manifest.a2e9430bde68bcb15352c2dbe9ffb2b4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112daed341863cd01bbe",
+    "url": "/css/app.7e51dbfc.css"
+  },
+  {
+    "revision": "cc9e3a84f9b9b5bdad45",
+    "url": "/css/chunk-vendors.fd869ed7.css"
+  },
+  {
+    "revision": "bb80656765d7dfb0c61f62663007dae3",
+    "url": "/img/About-Us.bb806567.png"
+  },
+  {
+    "revision": "c46ffe4ccdab4eb4d1ce3f027458b20d",
+    "url": "/img/Card-big-back.c46ffe4c.png"
+  },
+  {
+    "revision": "2c44d96504f34e101548281f89b12881",
+    "url": "/img/Recent-Works-Card1.2c44d965.png"
+  },
+  {
+    "revision": "097309be6f113d7edd07d63e85a7d1d3",
+    "url": "/img/Recent-Works-Card2.097309be.png"
+  },
+  {
+    "revision": "ee43e83796b5d2ba70228c6d7cea5aba",
+    "url": "/img/Recent-Works-Card3.ee43e837.png"
+  },
+  {
+    "revision": "45c9769409430b7e1d000e9ff90354a4",
+    "url": "/img/Recent-Works-back.45c97694.png"
+  },
+  {
+    "revision": "de7f767ece4c030b49eae7ed80d98b62",
+    "url": "/img/Site-Hero-Back.de7f767e.png"
+  },
+  {
+    "revision": "496ca6e95250604ce5a2037df37faa6a",
+    "url": "/img/Team-memb1.496ca6e9.png"
+  },
+  {
+    "revision": "f1f8bddf9f9082eeaaff59b44d886283",
+    "url": "/img/Team-memb2.f1f8bddf.png"
+  },
+  {
+    "revision": "7fe8259880843a49d5e366d675e914a3",
+    "url": "/img/Team-memb3.7fe82598.png"
+  },
+  {
+    "revision": "e73bdc0c6ff31138a63ec04f6b661508",
+    "url": "/img/Team-memb4.e73bdc0c.png"
+  },
+  {
+    "revision": "eaa82da26aee3a5b7e8c64a27454821a",
+    "url": "/img/Team-memb5.eaa82da2.png"
+  },
+  {
+    "revision": "e97eb980821b360966eb82503d77c563",
+    "url": "/img/Team-memb6.e97eb980.png"
+  },
+  {
+    "revision": "8942f6795fa51142726cbcb0117870b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "112daed341863cd01bbe",
+    "url": "/js/app.23b1373f.js"
+  },
+  {
+    "revision": "cc9e3a84f9b9b5bdad45",
+    "url": "/js/chunk-vendors.3b70bf94.js"
+  },
+  {
+    "revision": "af095f148ec3152f350eaa6ec95efda0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
